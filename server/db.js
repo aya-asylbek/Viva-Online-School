1f1e89c106622cf283cb39774cd070def4928a62
aichurek0990 <aichurek0990@gmail.com>
2025-10-24 16:30:59 -0700

    Configure database connection with environment variables

--- server/db.js
+++ server/db.js
@@ -1,12 +1,16 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: "aichurekasylbek",
-  host: "localhost",
-  database: "viva_school",
-  password: "",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.connect()
